cb93293b7676e8aeb5ee822d54770655fa636594
cahitsertunc.selen <cahitsertunc.selen@CSSELEN>
2023-05-31 16:48:22 +0300

    add composables

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
+<!-- -https://icones.js.org
+-https://vueuse.org/ -->
+
 <template>
     <div>home index page</div>
     <Alert />
     <Profile />
     <IconsBell />
 </template>
+
+<script setup>
+const { sayHello } = useUtils();
+sayHello();
+</script>
